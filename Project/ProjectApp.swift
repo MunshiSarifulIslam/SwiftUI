894083d182ddf72fb166a2335bd40082003da510
MunshiSarifulIslam <munshsarif176@gmail.com>
2024-02-07 16:45:15 +0530

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Consultant on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
